700ec26e9ec9839515be875a4d94ceebf3a68fe1
Jhonny Perez <jhonny.perez@gamasoftcol.com>
2019-08-16 17:21:56 -0500

    commit2

--- src/app/services/rutas.service.ts
+++ src/app/services/rutas.service.ts
@@ -9,15 +9,15 @@ export class RutasService {
 
   getPath() {
     // RUTA PARA  DESARROLLO LOCAL
-    const path = '/backgama/ptigGama/';
+    // const path = '/backgama/ptigGama/';
     //const path = 'http://localhost:8080/backgama/ptigGama/';
 
     // RUTA PARA AMBIENTE DESARROLLO
     //const path = 'http://gd3.gamasoftcol.com:8081/backgama_dev/ptigGama_dev/';
 
     // RUTA PARA AMBIENTE PRODUCCION
-    //const path = 'http://gd3.gamasoftcol.com:8081/backgama/ptigGama/';
-    return path;
+      const path = 'http://gd3.gamasoftcol.com:8081/backgama/ptigGama/';
+      return path;
   }
 
   
